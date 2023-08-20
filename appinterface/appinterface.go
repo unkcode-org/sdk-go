215bf3158f3518372114551570c07068df696e12
Unknowns24 <unknowns0074@gmail.com>
2023-08-20 19:09:23 -0300

    change package structure to adecuate to go package conventions

--- appinterface/appinterface.go
+++ appinterface/appinterface.go
@@ -2,13 +2,13 @@ package appinterface
 
 import "github.com/unkcode-org/sdk-go/auth"
 
-type AppInterface struct {
+type UNKCodeAppInterface struct {
 	Name                       string
 	Token                      string
 	RegularVerification        bool
 	FailedVerificationCallback func()
 }
 
-func (app *AppInterface) Login(license string, macAddress string) bool {
+func (app *UNKCodeAppInterface) Login(license string, macAddress string) bool {
 	return auth.LoginWithLicense(app.Token, app.Name, app.RegularVerification, license, macAddress, app.FailedVerificationCallback)
 }
